44f71b66dfe24e6f294da8dc2e334a23874fe2ed
Emil-Skovbo <emilskovbo18@gmail.com>
2019-09-12 17:49:12 +0200

    henter update

--- src/main/webapp/JSCode/Joke.js
+++ src/main/webapp/JSCode/Joke.js
@@ -35,3 +35,34 @@ function JokeToHTMLTable(arr) {
 }
 
 LoadJoke();
+
+
+document.getElementById("btn").onclick = RandomJoke;
+function RandomJoke() {
+    let url = document.location.origin + "/jpareststarter/api/Joke/"+ Math.floor((Math.random() * 3) + 1);;
+    fetch(url)
+            .then(res => res.json()) //get at json array 
+            .then(data => {
+                // Inside this callback, and only here, the response data is available
+                console.log("data", data);
+                document.getElementById('RandomJokeBox').innerHTML = RandomJokeToHTMLTable(data);
+            });
+
+}
+function RandomJokeToHTMLTable(arr) {
+    var arrHTML = arr.map(item => "<tr>"
+                + "<td>" + item.name + "</td>"
+                + "<td>" + item.id + "</td>"
+                + "<td>" + item.description + "</td>"
+                + "<td>" + item.color + "</td>"
+                + "</tr>");
+    var arrStr = arrHTML.join('');
+    var result = "<table class=\"table\"><tr>"
+            + "<th width = 100px>Name</th>"
+            + "<th width = 100px>Joke.nr</th>"
+            + "<th width = 100px>Description</th>"
+            + "<th width = 100px>Color</th>"
+            + arrStr + "</table>";
+    return result;
+}
+RandomJoke();
