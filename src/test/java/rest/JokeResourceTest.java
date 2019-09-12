cb1c3a27385c1e59f29a16b3b016a3e72a5b73a9
A O <ahmedothman8@hotmail.com>
2019-09-12 13:28:15 +0200

    fredd added the jokesresourcetest class

---
+++ src/test/java/rest/JokeResourceTest.java
@@ -0,0 +1,140 @@
+package rest;
+
+import entities.GroupMember;
+import entities.Joke;
+import facades.JokeFacade;
+import facades.MemberFacade;
+import utils.EMF_Creator;
+import io.restassured.RestAssured;
+import static io.restassured.RestAssured.given;
+import static io.restassured.RestAssured.when;
+import io.restassured.parsing.Parser;
+import java.net.URI;
+import java.util.List;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.ws.rs.core.UriBuilder;
+import org.glassfish.grizzly.http.server.HttpServer;
+import org.glassfish.grizzly.http.util.HttpStatus;
+import org.glassfish.jersey.grizzly2.httpserver.GrizzlyHttpServerFactory;
+import org.glassfish.jersey.server.ResourceConfig;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.hasItem;
+import static org.hamcrest.Matchers.hasItems;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import utils.EMF_Creator.DbSelector;
+import utils.EMF_Creator.Strategy;
+
+//Uncomment the line below, to temporarily disable this test
+//@Disabled
+public class JokeResourceTest {
+
+    private static final int SERVER_PORT = 7777;
+    private static final String SERVER_URL = "http://localhost/api";
+    //Read this line from a settings-file  since used several places
+    private static final String TEST_DB = "jdbc:mysql://localhost:3307/startcode_test";
+
+    static final URI BASE_URI = UriBuilder.fromUri(SERVER_URL).port(SERVER_PORT).build();
+    private static HttpServer httpServer;
+    private static EntityManagerFactory emf;
+    private static JokeFacade facade;
+    private Joke j1 = new Joke("Frøen", "Grøn","Ribbit sagde frøen haha");
+    private Joke j2 = new Joke("Giraf", "Gul","Langhals");
+    private Joke j3 = new Joke("Isterning", "rød","Hvorfor har en isterning hverken arme eller ben? Den er vanskabt.");
+
+    static HttpServer startServer() {
+        ResourceConfig rc = ResourceConfig.forApplication(new ApplicationConfig());
+        return GrizzlyHttpServerFactory.createHttpServer(BASE_URI, rc);
+    }
+
+    @BeforeAll
+    public static void setUpClass() {
+        emf = EMF_Creator.createEntityManagerFactory(DbSelector.TEST, Strategy.CREATE);
+
+        //NOT Required if you use the version of EMF_Creator.createEntityManagerFactory used above        
+        //System.setProperty("IS_TEST", TEST_DB);
+        //We are using the database on the virtual Vagrant image, so username password are the same for all dev-databases
+        httpServer = startServer();
+
+        //Setup RestAssured
+        RestAssured.baseURI = SERVER_URL;
+        RestAssured.port = SERVER_PORT;
+
+        RestAssured.defaultParser = Parser.JSON;
+    }
+
+    @AfterAll
+    public static void closeTestServer() {
+        //System.in.read();
+        httpServer.shutdownNow();
+    }
+
+    // Setup the DataBase (used by the test-server and this test) in a known state BEFORE EACH TEST
+    //TODO -- Make sure to change the script below to use YOUR OWN entity class
+    @BeforeEach
+    public void setUp() {
+        EntityManager em = emf.createEntityManager();
+        try {
+            em.getTransaction().begin();
+            em.createNamedQuery("Joke.deleteAllRows").executeUpdate();
+            em.persist(j1);
+            em.persist(j2);
+            em.persist(j3);
+
+            em.getTransaction().commit();
+        } finally {
+            em.close();
+        }
+    }
+
+    @Test
+    public void testServerIsUp() {
+        System.out.println("Testing is server UP");
+        given().when().get("/Joke").then().statusCode(200);
+    }
+
+    @Test
+    public void testCount() throws Exception {
+        given()
+                .contentType("application/json")
+                .get("/Joke/count").then()
+                .assertThat()
+                .statusCode(HttpStatus.OK_200.getStatusCode())
+                .body("count", equalTo(3));
+    }
+
+    /**
+     * This method is checking the all method by seeing if the names below exist on the list
+     */
+    @Test
+    public void testJokeIsOnList() {
+        given()
+                .contentType("application/json")
+                .get("/Joke/all").then()
+                .assertThat()
+                .statusCode(HttpStatus.OK_200.getStatusCode())
+                .body("name", hasItems("Frøen", "Giraf", "Isterning"));
+        System.out.println(j2);
+    }
+    
+
+    /**
+     * This method is testing if it is possible to get a member by typing their name
+     * As you can see below the name is simone and to see if it gets the right one we are checking if simones color is gul
+     */
+    @Test
+    public void getMemberByNameTest() {
+
+        when().
+                get("/Joke/name/Giraf").
+                then().
+                statusCode(200).
+                body("color", hasItem("Gul"));
+    }
+    
+    
+}
